8c1235c970eec17a299f80fd4c98ceb8aae1fe74
mehulghosal <mehul.ghosal@gmail.com>
2019-01-25 00:06:47 -0500

    setup

---
+++ pianotiles.py
@@ -0,0 +1,14 @@
+import math, sys, time
+from PIL import Image, ImageGrab
+
+def click(x, y):
+    pass
+
+#top left (x1, y1), bottom right (x2, y2)
+#returns average value of pixels
+def checkPixel(x1, y1, x2, y2):
+    pass
+
+#main game loop in here
+if __name__ == 'main':
+    pass
