44259d61599125206204b8c3f2629af960336b2c
mehulghosal <mehul.ghosal@gmail.com>
2019-03-24 00:17:38 -0400

    sublime's new update is annoying

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pianotilesbot
 god im bored and should be studying
+
+heh dont expect much in awhile. youll know im free when i add to this
--- pianotiles.py
+++ pianotiles.py
@@ -9,6 +9,10 @@ def click(x, y):
 def checkPixel(x1, y1, x2, y2):
     pass
 
+#y co: 560 --> 710, 415 --> 555, 415 --> 265
+#x co for bottom squares (add 97): (595, 710) --> (692, 710), 695 --> 792, 795 --> 892, 895 --> 992
+
+
 #main game loop in here
 if __name__ == 'main':
     pass
